d26a089634755361656fa43324ecf8f9da6a7cc1
virvaiva <103940964+virvaiva@users.noreply.github.com>
2022-09-11 12:58:00 +0000

    Deploying to gh-pages from @ virvaiva/goit-js-hw-09@dbd564f4bc259901a1d3f2dab0b2c9b885ea54d9 🚀

---
+++ 01-color-switcher.bfe6bac3.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");let n=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)}));
+//# sourceMappingURL=01-color-switcher.bfe6bac3.js.map
